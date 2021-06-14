0db57a848a31572e81a07e99462f6009aad4c80d
Erick Murillo <erickmurillo22@gmail.com>
2021-06-14 10:30:35 -0600

    terminando ejemplo

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 import 'package:chewie/chewie.dart';
 import 'package:video_player/video_player.dart';
-import 'package:dio/dio.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -60,10 +59,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final imgUrl =
-      "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4";
   bool downloading = false;
   var progressString = "";
+  String urlVideo;
 
   VideoPlayerController _videoPlayerController2;
   ChewieController _chewieController;
@@ -71,21 +69,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    // _videoPlayerController2 = VideoPlayerController.network(
-    //     'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
-    // final _videoPlayerController2 =
-    //     VideoPlayerController.asset('/Downloads/video360p.mp4');
-    // _videoPlayerController2 = VideoPlayerController.asset(
-    //     '/storage/emulated/0/Download/FZ22abbMuJg.webm');
-    final File file = File('/storage/emulated/0/Download/FZ22abbMuJg.mp4');
+    _loadPath();
+  }
 
-    _videoPlayerController2 = VideoPlayerController.file(file);
+  void _loadPath() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      urlVideo = (prefs.getString('path') ?? '');
+      final File fileDir = File(urlVideo);
 
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController2,
-      autoPlay: true,
-      looping: true,
-    );
+      _videoPlayerController2 = VideoPlayerController.file(fileDir);
+
+      _chewieController = ChewieController(
+        videoPlayerController: _videoPlayerController2,
+        autoPlay: true,
+        looping: true,
+        aspectRatio: 16 / 9,
+      );
+    });
   }
 
   @override
@@ -95,35 +96,99 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-//   Future<void> downloadFile() async {
-//     Dio dio = Dio();
+  Future<void> download() async {
+    //   downloadFile();
+    var yt = YoutubeExplode();
+    var id = 'https://www.youtube.com/watch?v=tC7fW5HpEU4';
+    var video = await yt.videos.get(id);
 
-//     try {
-//       //   var dir = await getApplicationDocumentsDirectory();
-//       await Permission.storage.request();
-//       var dir = await DownloadsPathProvider.downloadsDirectory;
+    // Display info about this video.
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text('Title: ${video.title}, Duration: ${video.duration}'),
+        );
+      },
+    );
 
-//       print("path ${dir.path}");
-//       await dio.download(imgUrl, "${dir.path}/demo.mp4",
-//           onReceiveProgress: (rec, total) {
-//         // print("Rec: $rec , Total: $total");
+    // Request permission to write in an external directory.
+    // (In this case downloads)
+    await Permission.storage.request();
 
-//         setState(() {
-//           downloading = true;
-//           progressString = ((rec / total) * 100).toStringAsFixed(0) + "%";
-//         });
-//       });
-//       path = "${dir.path}/demo.mp4";
-//     } catch (e) {
-//       print(e);
-//     }
+    // Get the streams manifest and the audio track.
+    var manifest = await yt.videos.streamsClient.getManifest(id);
+    var audio = manifest.muxed.first;
 
-//     setState(() {
-//       downloading = false;
-//       progressString = "Completed";
-//     });
-//     print("Download completed");
-//   }
+    // Build the directory.
+    var dir = await DownloadsPathProvider.downloadsDirectory;
+    var filePath =
+        path.join(dir.uri.toFilePath(), '${video.id}.${audio.container.name}');
+
+    // Open the file to write.
+    var file = File(filePath);
+    var fileStream = file.openWrite();
+
+    // Pipe all the content of the stream into our file.
+    var audioStream = yt.videos.streamsClient.get(audio);
+    // Track the file download status.
+    var len = audio.size.totalBytes;
+    var count = 0;
+
+    await for (final data in audioStream) {
+      // Keep track of the current downloaded data.
+      count += data.length;
+
+      // Calculate the current progress.
+      // var progress = ((count / len) * 100).ceil();
+
+      // Update the progressbar.
+      // progressBar.update(progress);
+
+      // Write to file.
+      fileStream.add(data);
+
+      setState(() {
+        downloading = true;
+        progressString = ((count / len) * 100).toStringAsFixed(0) + "%";
+      });
+    }
+    //   await audioStream.pipe(fileStream);
+
+    // Close the file.
+    await fileStream.flush();
+    await fileStream.close();
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('path', '${filePath}');
+    print(prefs.getString('path'));
+
+    setState(() {
+      downloading = false;
+      progressString = "Completed";
+      urlVideo = '${filePath}';
+      final File fileDir = File(urlVideo);
+
+      _videoPlayerController2 = VideoPlayerController.file(fileDir);
+
+      _chewieController = ChewieController(
+        videoPlayerController: _videoPlayerController2,
+        autoPlay: true,
+        looping: true,
+        aspectRatio: 16 / 9,
+      );
+    });
+
+    // Show that the file was downloaded.
+    // await showDialog(
+    //   context: context,
+    //   builder: (context) {
+    //     return AlertDialog(
+    //       content: Text('Download completed and saved to: ${filePath}'),
+    //     );
+    //   },
+    // );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -131,71 +196,15 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
                 onPressed: () async {
-                  //   downloadFile();
-                  var yt = YoutubeExplode();
-                  var id = 'qKOPl7PHvfw';
-                  var video = await yt.videos.get(id);
-
-                  // Display info about this video.
-                  await showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: Text(
-                            'Title: ${video.title}, Duration: ${video.duration}'),
-                      );
-                    },
-                  );
-
-                  // Request permission to write in an external directory.
-                  // (In this case downloads)
-                  await Permission.storage.request();
-
-                  // Get the streams manifest and the audio track.
-                  var manifest = await yt.videos.streamsClient.getManifest(id);
-                  var audio = manifest.muxed.first;
-
-                  // Build the directory.
-                  var dir = await DownloadsPathProvider.downloadsDirectory;
-                  var filePath = path.join(dir.uri.toFilePath(),
-                      '${video.id}.${audio.container.name}');
-
-                  // Open the file to write.
-                  var file = File(filePath);
-                  var fileStream = file.openWrite();
-
-                  // Pipe all the content of the stream into our file.
-                  await yt.videos.streamsClient.get(audio).pipe(fileStream);
-                  /*
-                  If you want to show a % of download, you should listen
-                  to the stream instead of using `pipe` and compare
-                  the current downloaded streams to the totalBytes,
-                  see an example ii example/video_download.dart
-                   */
-
-                  // Close the file.
-                  await fileStream.flush();
-                  await fileStream.close();
-
-                  // Show that the file was downloaded.
-                  await showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        content: Text(
-                            'Download completed and saved to: ${filePath}'),
-                      );
-                    },
-                  );
+                  download();
                 },
                 child: Text('Descargar')),
-
             Center(
               child: downloading
                   ? Container(
@@ -220,15 +229,16 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       ),
                     )
-                  : Text("No Data"),
+                  : urlVideo != ''
+                      ? Container(
+                          child: Center(
+                            child: Chewie(
+                              controller: _chewieController,
+                            ),
+                          ),
+                        )
+                      : Text('No data'),
             ),
-            // Expanded(
-            //   child: Center(
-            //     child: Chewie(
-            //       controller: _chewieController,
-            //     ),
-            //   ),
-            // ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
